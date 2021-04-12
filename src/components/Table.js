1e8d94404995c34c38f6ec48086b54a01a21779d
Sommer Moya-Mendez <smoyamendez@gmail.com>
2021-04-12 17:48:43 -0400

    added mailto: to email & fixed table error

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,29 +1,33 @@
 import React from 'react';
 
 function makeTable(props) {
-
+    const filterCursor = event => {
+        event.target.style.cursor = "pointer"
+    }
+    
+   
     return (
         <table className="table">
             <thead>
                 <tr>
                     <th scope="col">PHOTO</th>
-                    <th onClick={() => props.filterByProp()} scope="col">NAME</th>
+                    <th onClick={() => props.filterByProp()} onMouseOver={filterCursor} scope="col">NAME</th>
                     <th scope="col">PHONE</th>
                     <th scope="col">EMAIL</th>
                     <th scope="col">DOB</th>
                 </tr>
             </thead>
+            <tbody>
             {props.results.map(result => (
-                <tbody key={result.id.value}>
-
-                    <th scope="row"><img src={result.picture.thumbnail} alt="employee-img"></img></th>
-                    <td>{result.name.first} {result.name.last}</td>
-                    <td>{result.phone}</td>
-                    <td>{result.email}</td>
-                    <td>{new Date(result.dob.date).getMonth() + 1}/{new Date(result.dob.date).getDate()}/{new Date(result.dob.date).getFullYear()}</td>
-
-                </tbody>
+                <tr  key={result.id.value} >
+                <th scope="row"><img src={result.picture.thumbnail} alt="employee-img"></img></th>
+                <td>{result.name.first} {result.name.last}</td>
+                <td>{result.phone}</td>
+                <td><a href={`mailto:${result.email}`}>{result.email}</a></td>
+                <td>{new Date(result.dob.date).getMonth() + 1}/{new Date(result.dob.date).getDate()}/{new Date(result.dob.date).getFullYear()}</td>
+                </tr>
             ))}
+            </tbody>
         </table>
     )
 };
